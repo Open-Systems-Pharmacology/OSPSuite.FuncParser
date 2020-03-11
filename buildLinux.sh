d747ca3a4cd7b881d50395bcbdd86c85de9dd559
Yuri05 <Yuri05@github.com>
2020-03-11 13:20:39 +0100

    Adjusted linux build scriüpt so that the original solution file is not modified anymore

--- buildLinux.sh
+++ buildLinux.sh
@@ -3,16 +3,22 @@
 #call: buildLinux.sh distributionName version
 # e.g. buildLinux.sh CentOS7 4.0.0.49
 
+rm -f OSPSuite.FuncParser4Linux.sln
+
 dotnet restore --source https://ci.appveyor.com/nuget/ospsuite-utility --source https://ci.appveyor.com/nuget/ospsuite-bddhelper --source https://www.nuget.org/api/v2/
-dotnet sln OSPSuite.FuncParser.sln remove src/OSPSuite.FuncParserNative/OSPSuite.FuncParserNative.vcxproj
+
+# copy the original solution file because it will be modified for dotnet build
+cp -p -f OSPSuite.FuncParser.sln OSPSuite.FuncParser4Linux.sln
+
+dotnet sln OSPSuite.FuncParser4Linux.sln remove src/OSPSuite.FuncParserNative/OSPSuite.FuncParserNative.vcxproj
 
 cmake -BBuild/Release/x64/ -Hsrc/OSPSuite.FuncParserNative/ -DCMAKE_BUILD_TYPE=Release
 make -C Build/Release/x64
-dotnet build OSPSuite.FuncParser.sln /property:Configuration=Release;Platform=x64
+dotnet build OSPSuite.FuncParser4Linux.sln /property:Configuration=Release;Platform=x64
 
 cmake -BBuild/Debug/x64/ -Hsrc/OSPSuite.FuncParserNative/ -DCMAKE_BUILD_TYPE=Debug
 make -C Build/Debug/x64
-dotnet build OSPSuite.FuncParser.sln /property:Configuration=Debug;Platform=x64
+dotnet build OSPSuite.FuncParser4Linux.sln /property:Configuration=Debug;Platform=x64
 
 # dotnet test --no-build --no-restore --configuration:Release #optionally: run tests
 
