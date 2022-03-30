4a644ebefd1aaaf072cfe7a50444b42b185c5fdd
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-03-30 14:38:11 +0200

    simplify capitalize helper

--- src/OSPSuite.FuncParserNative/src/StringHelper.cpp
+++ src/OSPSuite.FuncParserNative/src/StringHelper.cpp
@@ -6,14 +6,10 @@ namespace FuncParserNative
 
 std::string StringHelper::Capitalize (const std::string & pInString)
 {
-	char * NewStr = new char[pInString.size()+1];
-	const char *csource = pInString.c_str();
-    for(size_t i=0; i<pInString.size(); i++)
-		NewStr[i] = i==0 ? toupper(csource[i]) : tolower(csource[i]);
-	NewStr[pInString.size()] = '\0';
-	std::string newString = NewStr;
-	delete[] NewStr;
-	return newString;
+    std::string newString = pInString;
+    newString[0] = toupper(pInString[0]);
+
+    return newString;
 }
 
 }//.. end "namespace FuncParserNative"
