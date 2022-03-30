3581151accf50db15ec9da499a1620dba057454b
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-03-30 14:57:02 +0200

    make sure everything else is lowercase

--- src/OSPSuite.FuncParserNative/src/StringHelper.cpp
+++ src/OSPSuite.FuncParserNative/src/StringHelper.cpp
@@ -1,5 +1,6 @@
 #include "FuncParser/StringHelper.h"
 #include <string>
+#include <algorithm>
 
 namespace FuncParserNative
 {
@@ -8,6 +9,7 @@ std::string StringHelper::Capitalize (const std::string & pInString)
 {
     std::string newString = pInString;
     newString[0] = toupper(pInString[0]);
+    std::transform(newString.begin() + 1, newString.end(), newString.begin() + 1, ::tolower);
 
     return newString;
 }
