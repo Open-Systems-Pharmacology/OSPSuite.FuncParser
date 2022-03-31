532b6d8cc52f67dccefbc794dd7175f62d86c099
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-03-31 19:14:55 +0200

    Simplify string helper (#49)
    * simplify capitalize helper
    
    * make sure everything else is lowercase
    
    * comments
    
    * protect against empty string input

--- src/OSPSuite.FuncParserNative/src/StringHelper.cpp
+++ src/OSPSuite.FuncParserNative/src/StringHelper.cpp
@@ -1,13 +1,25 @@
 #include "FuncParser/StringHelper.h"
 #include <string>
+#include <algorithm>
 
 namespace FuncParserNative
 {
 
 std::string StringHelper::Capitalize (const std::string & pInString)
 {
+    // return early if the string is empty; nothing to change in this case
+    if (pInString.empty())
+    {
+        return pInString;
+    }
+
     std::string newString = pInString;
+
+    // *only* the first character should be uppercase
     newString[0] = toupper(pInString[0]);
+
+    // all other characters should be changed to lowercase
+    std::transform(newString.begin() + 1, newString.end(), newString.begin() + 1, ::tolower);
 
     return newString;
 }
